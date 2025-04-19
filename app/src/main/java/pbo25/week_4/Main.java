0f531e0cb5eda90c275920f85e4f2ffd1ec39224
maismdyh <maisyahmahdiyyah@gmail.com>
2025-04-20 02:44:32 +0800

    `feat`: tugas praktikum 4

--- app/src/main/java/pbo25/week_4/Main.java
+++ app/src/main/java/pbo25/week_4/Main.java
@@ -36,6 +36,7 @@ public class Main {
             case 2:
                 // Membuka Halaman Register
                 showRegisterMenu();
+                break;
             default:
                 // Mengulang Pemanggilan Menu Utama
                 runApp();
@@ -54,16 +55,11 @@ public class Main {
         // Membuat variabel userIndex yang mana nanti akan menampung index dari user
         int userIndex = -1;
         for (int i = 0; i < listUser.size(); i++) {
-            /*
-             * TODO
-             * Buatlah sebuah perkondisian (IF) yang akan
-             * Mengecek apakah user index ke i dari listUser memiliki username yang sama
-             * dengan username yang
-             * diinput.
-             * Jika ada ganti userIndex dengan Index dari User Tersebut, kemudian hentikan
-             * perulangan
-             */
-        }
+            if (listUser.get(i).getUsername().equals(username)) {
+                userIndex = i;
+                break;
+            }
+        }        
         // Saat userIndex tidak sama dengan -1 atau userIndexnya ditemukan
         if (userIndex != -1) {
             // Menginput Password
@@ -71,26 +67,21 @@ public class Main {
             System.out.print("> ");
             String password = sc.next();
 
-            /*
-             * TODO
-             * Mengecek apakah password dari User yang login berdasarkan username
-             * sama dengan password yang diinput sebelumnya, kemudian simpan
-             * hasilnya di variabel isPasswordMatch
-             */
+            boolean isPasswordMatch = listUser.get(userIndex).getPassword().equals(password);
 
             // Jika passwordnya sama maka berhasil login
             if (isPasswordMatch) {
                 System.out.println("Berhasil Login");
-                /*
-                 * TODO
-                 * panggil method showDetailUser dan kirimkan data Profile User yang login
-                 * 
-                 */
+
+                showDetailUser(listUserProfile.get(userIndex));
+
                 System.exit(0);
             } else {
                 // saat password salah akan menampikan password salah
                 System.out.println("Password Salah");
             }
+        } else {
+            System.out.println("Username tidak ditemukan.");
         }
     }
 
@@ -105,58 +96,58 @@ public class Main {
         System.out.println("Password");
         System.out.print("> ");
         String password = sc.nextLine();
-        /*
-         * TODO
-         * Buatlah atau Instance objek User baru, dan tambahkan
-         * username dan password yang diinput sebelumnya secara langsung
-         * saat instance User
-         */
 
-        /*
-         * TODO
-         * Buatlah atau Instance objek Profile baru
-         */
+        // Membuat objek User
+        User user = new User(username, password);
+
+        // Membuat objek Profile
         Profile profile = new Profile();
 
         // Menginput Data Profile
         System.out.println("Nama Lengkap");
         System.out.print("> ");
         String fullName = sc.nextLine();
+        profile.setFullName(fullName);
+
         System.out.println("Umur");
         System.out.print("> ");
         int age = sc.nextInt();
         sc.nextLine();
+        profile.setAge(age);
+
         System.out.println("Hobby");
         System.out.print("> ");
         String hobby = sc.nextLine();
+        profile.setHobby(hobby);
 
-        /*
-         * TODO
-         * Berikan nilai fullName, age, dan hobby ke objek profile yang telah
-         * di Instance sebelumnya. Nilai ini diperoleh dari data profile yang
-         * diinput sebelumnya
-         */
+        String nickName = StringUtils.generateNickName(fullName);
+        profile.setNickName(nickName);
 
-        /*
-         * TODO
-         * Berikan nilai nickName ke objek profile,
-         * Nilai ini diperoleh menggunakan static method yang dibuat di class
-         * StringUtils, dengan mengirimkan fullName yang diinput sebelumnya
-         */
+        // Menampilkan detail profile yang sudah diinput
+        System.out.println("-------------------------");
+        System.out.println("Detail Profile Pengguna");
+        System.out.println("Nama Lengkap : " + profile.getFullName());
+        System.out.println("Nick Name    : " + profile.getNickName());
+        System.out.println("Umur         : " + profile.getAge());
+        System.out.println("Hobby        : " + profile.getHobby());
 
         // Menambahkan user yang dibuat ke list user
         listUser.add(user);
         // Menambahkan profile user yang dibuat ke list profile
         listUserProfile.add(profile);
+
         System.out.println("-------------------------");
         System.out.println("Berhasil Membuat User Baru!!");
         runApp();
     }
 
     private static void showDetailUser(Profile profile) {
-        /*
-         * TODO *
-         * Tampilkan semua data profile user yang login
-         */
+        System.out.println("-------------------------");
+        System.out.println("Detail Pengguna");
+        System.out.println("-------------------------");
+        System.out.println("Nama Lengkap : " + profile.getFullName());
+        System.out.println("Nick Name    : " + profile.getNickName());
+        System.out.println("Umur         : " + profile.getAge());
+        System.out.println("Hobby        : " + profile.getHobby());
     }
 }
--- app/src/main/java/pbo25/week_4/utils/StringUtils.java
+++ app/src/main/java/pbo25/week_4/utils/StringUtils.java
@@ -1,15 +1,12 @@
 package pbo25.week_4.utils;
 
 public class StringUtils {
-    /*
-     TODO
-     * Buatlah sebuah method static yang akan
-     * mengembalikan nickName berdasarkan fullName yang diberikan
-     * aturan pembuatan nickName adalah :
-     * 1. Jika fullName hanya 1 kata maka nickName = fullName
-     * (ex: FullName = Agus, maka NickName = Agus)
-     * 2. Jika fullName lebih dari 1 kata maka nickName adalah kata kedua dari
-     * fullName
-     * (ex: FullName = Eurico Devon, maka NickName = Devon)
-     */
+    public static String generateNickName(String fullName) {
+        String[] parts = fullName.trim().split(" ");
+        if (parts.length == 1) {
+            return parts[0];
+        } else {
+            return parts[1];
+        }
+    }
 }
