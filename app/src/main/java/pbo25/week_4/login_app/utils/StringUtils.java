48bc808d54b6e98769d8bb3dfea94192ae9cdb30
Haris2512 <harissaputra11736@gmail.com>
2025-04-21 14:47:36 +0800

    Memperbaiki kode class StringUtils karna sebelumnya tidak menggunakan pengkondisian

--- app/src/main/java/pbo25/week_4/login_app/utils/StringUtils.java
+++ app/src/main/java/pbo25/week_4/login_app/utils/StringUtils.java
@@ -1,11 +1,11 @@
-package login_app.utils;
+package pbo25.week_4.login_app.utils;
 
 public class StringUtils {
     public static String generateNickName(String fullName) {
         if (fullName == null || fullName.trim().isEmpty()) {
-            return "unknown";
+            throw new IllegalArgumentException("FullName cannot be null or empty");
         }
-        String[]names = fullName.trim().split("\\s+");
+        String[] names = fullName.trim().split("\\s+");
         if (names.length == 1) {
             return fullName.trim();
         }
