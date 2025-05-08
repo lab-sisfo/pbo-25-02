78c0166f2b58e93703316cea8dec8ebeab3ef2e2
Asenictzy <kevinsomakila@gmail.com>
2025-05-09 00:16:53 +0800

    'feat':tugas praktikum 6

---
+++ app/src/main/java/pbo25/week_6/IServiceable.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+public interface IServiceable {
+    boolean periksaKondisi();
+    void lakukanServis();
+    Date getWaktuServisBerikutnya();
+    double hitungBiayaServis();
+}
